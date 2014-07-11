610cc165850ea7000d819b6ea6e198c9c20cc300
Andy Truong <thehongtt@gmail.com>
2014-07-11 09:04:06 +0700

    Update bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -1,13 +1,19 @@
 <?php
 
-
 use Symfony\Component\ClassLoader\UniversalClassLoader;
 
-$composer_loader = require_once __DIR__ . '/../../autoload.php';
+$autoload_file[] = __DIR__ . '/../../autoload.php';
+$autoload_file[] = __DIR__ . '/vendor/autoload.php';
+
+foreach ($autoload_file as $file) {
+    if (is_file($file)) {
+        $composer_loader = require $file;
+        $composer_loader->addPsr4('AndyTruong\\PHPClassTree\\', __DIR__ . '/src');
+        break;
+    }
+}
+
 $config = require_once __DIR__ . '/config/config.php';
-
-$composer_loader->addPsr4('AndyTruong\\PHPClassTree\\', __DIR__ . '/src');
-
 $loader = new UniversalClassLoader();
 foreach ($config['autoload'] as $load_type => $mapping) {
     foreach ($mapping as $namespace => $paths) {
