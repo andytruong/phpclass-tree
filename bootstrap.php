41c650c1f6a2be253773676b82a817fa3483a67c
Andy Truong <thehongtt@gmail.com>
2014-08-25 17:44:39 +0700

    Use composer_loader only.

--- bootstrap.php
+++ bootstrap.php
@@ -1,12 +1,16 @@
 <?php
 
+use Composer\Autoload\ClassLoader;
+use Symfony\Component\ClassLoader\Psr4ClassLoader;
 use Symfony\Component\ClassLoader\UniversalClassLoader;
 
 $autoload_file[] = __DIR__ . '/../../autoload.php';
 $autoload_file[] = __DIR__ . '/vendor/autoload.php';
+$autoload_file[] = __DIR__ . '/../vendor/autoload.php';
 
 foreach ($autoload_file as $file) {
     if (is_file($file)) {
+        /* @var $composer_loader ClassLoader */
         $composer_loader = require $file;
         $composer_loader->addPsr4('AndyTruong\\PHPClassTree\\', __DIR__ . '/src');
         break;
@@ -14,16 +18,17 @@ foreach ($autoload_file as $file) {
 }
 
 $config = require_once __DIR__ . '/config/config.php';
-$loader = new UniversalClassLoader();
 foreach ($config['autoload'] as $load_type => $mapping) {
     foreach ($mapping as $namespace => $paths) {
         switch ($load_type) {
             case 'psr-0':
-                $loader->registerNamespace($namespace, $paths);
+                $composer_loader->add($namespace, $paths);
+                break;
+            case 'psr-4':
+                $composer_loader->addPsr4($namespace, $paths);
                 break;
         }
     }
 }
-$loader->register();
 
-return $loader;
+return $composer_loader;
