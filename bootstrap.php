073e5b48218071949c7e98dc232fe6c70a9f646b
Andy Truong <thehongtt@gmail.com>
2014-08-26 08:16:37 +0700

    misc

--- bootstrap.php
+++ bootstrap.php
@@ -1,18 +1,16 @@
 <?php
 
 use Composer\Autoload\ClassLoader;
-use Symfony\Component\ClassLoader\Psr4ClassLoader;
-use Symfony\Component\ClassLoader\UniversalClassLoader;
 
 $autoload_file[] = __DIR__ . '/../../autoload.php';
 $autoload_file[] = __DIR__ . '/vendor/autoload.php';
 $autoload_file[] = __DIR__ . '/../vendor/autoload.php';
 
+/* @var $loader ClassLoader */
 foreach ($autoload_file as $file) {
     if (is_file($file)) {
-        /* @var $composer_loader ClassLoader */
-        $composer_loader = require $file;
-        $composer_loader->addPsr4('AndyTruong\\PHPClassTree\\', __DIR__ . '/src');
+        $loader = require $file;
+        $loader->addPsr4('AndyTruong\\PHPClassTree\\', __DIR__ . '/src');
         break;
     }
 }
@@ -22,13 +20,13 @@ foreach ($config['autoload'] as $load_type => $mapping) {
     foreach ($mapping as $namespace => $paths) {
         switch ($load_type) {
             case 'psr-0':
-                $composer_loader->add($namespace, $paths);
+                $loader->add($namespace, $paths);
                 break;
             case 'psr-4':
-                $composer_loader->addPsr4($namespace, $paths);
+                $loader->addPsr4($namespace, $paths);
                 break;
         }
     }
 }
 
-return $composer_loader;
+return $loader;
